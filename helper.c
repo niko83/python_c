61a894586961c392186ffcc6f7f3750d44644b9b
m_nikolayenko <m_nikolayenko@wargaming.net>
2016-08-12 14:45:14 +0300

    add resolve line

--- helper.c
+++ helper.c
@@ -1,4 +1,5 @@
 #include "math.h"
+#include <stdio.h>
 
 
 double helper_distance(float x1, float y1, float x2, float y2) 
@@ -6,3 +7,18 @@ double helper_distance(float x1, float y1, float x2, float y2)
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
 
+
+float * helper_resolve_line(float x1, float y1, float x2, float y2)
+{
+    if (x1 == x1){
+        float result[2] = {0, 0}; /* vertical */
+        return result;
+    }
+
+    float K, B;
+
+    K = (y2 - y1) / (x2 - x1);
+    B = y2 - K * x2;
+    float result[2] = {K, B};
+    return result;
+}
--- helper.h
+++ helper.h
@@ -1,1 +1,3 @@
 double helper_distance(float x1, float y1, float x2, float y2);
+
+float * helper_resolve_line(float x1, float y1, float x2, float y2);
