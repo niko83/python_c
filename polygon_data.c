840b71cd2f02297060aac2cb1559b8e99dabd5b9
niko <test@test.com>
2016-08-24 22:56:18 +0300

    update polygon_data

--- polygon_data.c
+++ polygon_data.c
@@ -1,4 +1,3 @@
-
 typedef struct {
     int x;
     int y;
@@ -13,20 +12,29 @@ typedef struct {
     int y_max;
 } Edge;
 
-static point_list polygon0 = {{0,0}, {100,0}, {100, 100}, {0, 100}};
-static point_list polygon1 = {{0,0}, {1000,0}, {1000, 1000}, {0, 1000}};
-static point_list polygon2 = {{1,2}, {10030,4}, {10050, 10060}, {7, 10080}};
 
-static Edge p0 = {0, 100, 0, 100};
-static Edge p1 = {0, 1000, 0, 1000};
-static Edge p2 = {0, 10000, 0, 10000};
+static point_list polygon_fake = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+static point_list polygon2 = {{0, 0}, {3371, 0}, {3371, 50}, {0, 20}};
+static point_list polygon3 = {{3371-50, 0}, {3371, 0}, {3371, 1889}, {3371-50, 1889}};
+static point_list polygon4 = {{0, 1889-50}, {3371, 1889-50}, {3371, 1889}, {0, 1889}};
+static point_list polygon5 = {{0, 0}, {50, 0}, {50, 1889}, {0, 1889}};
+
+static Edge p_fake  =   {0, 1, 0, 1};
+static Edge p2  =   {0, 3371, 0, 50};
+static Edge p3  =   {3321, 3371, 0, 1889};
+static Edge p4  =   {0, 3371, 1839, 1889};
+static Edge p5  =   {0, 50, 0, 1889};
+
+
 
 static struct {
   point_list *mtp;
   int el_count;
   Edge *edge;
 } all_polygons[] = {
-    {(point_list*)polygon0, sizeof(polygon0)/sizeof(polygon0[0]), &p0},
-    {(point_list*)polygon1, sizeof(polygon1)/sizeof(polygon1[0]), &p1},
-    {(point_list*)polygon2, sizeof(polygon2)/sizeof(polygon2[0]), &p2},
+  {(point_list*)polygon_fake,  sizeof(polygon_fake)/ sizeof(polygon_fake[0]) , &p_fake},
+  {(point_list*)polygon2,  sizeof(polygon2)/ sizeof(polygon2[0]) , &p2},
+  {(point_list*)polygon3,  sizeof(polygon3)/ sizeof(polygon3[0]) , &p3},
+  {(point_list*)polygon4,  sizeof(polygon4)/ sizeof(polygon4[0]) , &p4},
+  {(point_list*)polygon5,  sizeof(polygon5)/ sizeof(polygon5[0]) , &p5},
 };
